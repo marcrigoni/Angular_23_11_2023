ff43dd7f91c9769d49a8b38685770a0212c90c86
marcrigoni <marcrigoni@gmail.com>
2023-11-29 11:16:12 -0300

    Secção 7: Organizando Rotas, Alertas e Mais

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
@@ -19,15 +19,24 @@ import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 import { ModalModule } from "ngx-bootstrap/modal";
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+
 @NgModule({
   declarations: [
-      AppComponent,
-      EventosComponent,
-      EventosComponent,
-      PalestrantesComponent,
-      NavComponent,
-      DateTimeFormatPipe
-   ],
+    AppComponent,
+    EventosComponent,
+    EventosComponent,
+    PalestrantesComponent,
+    NavComponent,
+    DateTimeFormatPipe,
+    ContatosComponent,
+    DashboardComponent,
+    PerfilComponent,
+    TituloComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
